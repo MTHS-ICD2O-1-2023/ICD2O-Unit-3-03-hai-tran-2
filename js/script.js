d53721f6bbecdfe79b5c44fd9ef2cdb4f1b3fa21
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-05 18:26:47 +0000

    added first wbpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of sphere
+ */
+function calculateVolume() {
+  // input
+  const radiusOfSphere = parseFloat(document.getElementById('radius-of-sphere').value)
+  // process
+  const volumeOfSphere = (4 * Math.PI * radiusOfSphere * radiusOfSphere * radiusOfSphere) / 3
+
+  // output
+  document.getElementById('answer').innerHTML = "The volume is: " + volumeOfSphere.toFixed(2) + " mm³"
+}
